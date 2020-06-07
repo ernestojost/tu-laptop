cae0ca76003c7c2e2aa70ecde6c4c63d83a97bb4
Ernesto Jost <ernestojost@hotmail.com>
2020-06-07 13:34:59 -0300

    Agregado el favicon

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8" />
         <title>Tu Laptop</title>
-
+        <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+        
         <!-- Bootstrap -->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
